6e29878c79434f5d60b76b0e0b4d9ef7e1cd82b6
divmish-1007 <132588661+divmish-1007@users.noreply.github.com>
2024-07-23 15:54:02 +0530

    Time: 42 ms (57.51%), Space: 36.3 MB (57.02%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map<int, int> mpp;
+        
         vector<int> ans;
-        for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it=mpp.begin(); it!=mpp.end(); it++){
-            if(it->second > 1){
-                ans.push_back(it->first);
+        for(int i = 0; i< nums.size(); i++){
+            int index = abs(nums[i]) - 1;
+
+            if(nums[index] < 0){
+                ans.push_back(index+1);
             }
+            nums[index] = -nums[index];
         }
         return ans;
+        
+        
+        // unordered_map<int, int> mpp;
+        // vector<int> ans;
+        // for(int i=0; i<nums.size(); i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it=mpp.begin(); it!=mpp.end(); it++){
+        //     if(it->second > 1){
+        //         ans.push_back(it->first);
+        //     }
+        // }
+        // return ans;
     }
 };
