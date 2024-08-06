47748d6de5b461acf8ec5c7a6dbd74a9a30ce760
divmish-1007 <132588661+divmish-1007@users.noreply.github.com>
2024-08-06 19:23:42 +0530

    Time: 2 ms (49.59%), Space: 11.2 MB (41.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left = m-1;
-        int right = 0;
-
-        while(left>=0 && right<n){
-            if(nums1[left] > nums2[right]){
-                swap(nums1[left], nums2[right]);
-                left--;
-                right++;
-            }
-            else{
-                break;
-            }
-        }
-        sort(nums1.begin(), nums1.begin()+m);
-        sort(nums2.begin(), nums2.end());
-
-// Merged both arrays 
+        
+            
+// Gap Method
         for(int i=m; i<n+m; i++){
             nums1[i] = nums2[i-m];
         }
+
+        int len = n+m;
+        int gap = (len/2) + (len%2);
+
+        while(gap>0){
+            int left = 0; 
+            int right = left + gap;
+
+            while(right < (m+n)){
+                if(nums1[left] > nums1[right]){
+                    swap(nums1[left] , nums1[right]);
+                }
+                left++;
+                right++;
+            }
+            if(gap == 1) break;
+            gap = (gap/2) + (gap%2);
+        }
+    
+        
+        
+//         int left = m-1;
+//         int right = 0;
+
+//         while(left>=0 && right<n){
+//             if(nums1[left] > nums2[right]){
+//                 swap(nums1[left], nums2[right]);
+//                 left--;
+//                 right++;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         sort(nums1.begin(), nums1.begin()+m);
+//         sort(nums2.begin(), nums2.end());
+
+// // Merged both arrays 
+//         for(int i=m; i<n+m; i++){
+//             nums1[i] = nums2[i-m];
+//         }
     }
 };
